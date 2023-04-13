6919d660c57b85731a4a2fcd4ddd23e1f7f8cb44
Corey Schnedl <schnedl.2@osu.edu>
2023-04-13 12:47:25 -0400

    initial commit

---
+++ lib/lambdas/calculations/index.ts
@@ -0,0 +1,29 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { generateErrorResult, generateOkResult } from "./response";
+
+const addNumbers = (number1: number, number2: number): number => {
+  return number1 + number2;
+};
+
+// const subtractNumbers = (number1: number, number2: number): number => {
+//   return number1 - number2;
+// };
+
+export const handler = async (event: APIGatewayProxyEvent) => {
+  const { operation, number1, number2 } = JSON.parse(event.body || "{}");
+
+  let result;
+
+  switch (operation) {
+    case "ADD":
+      result = addNumbers(number1, number2);
+      break;
+    // case "SUBTRACT":
+    //   result = subtractNumbers(number1, number2);
+    //   break;
+    default:
+      return generateErrorResult();
+  }
+
+  return generateOkResult(JSON.stringify(result));
+};
---
+++ lib/lambdas/calculations/response.ts
@@ -0,0 +1,29 @@
+const headers = {
+  "Access-Control-Allow-Headers": "*",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "*",
+};
+
+const generateProxyIntegrationResponse = (
+  statusCode: number,
+  body?: string
+) => {
+  return {
+    statusCode,
+    body,
+    headers,
+  };
+};
+
+export const generateOkResult = (body: string) => {
+  return generateProxyIntegrationResponse(200, body);
+};
+
+export const generateErrorResult = () => {
+  return generateProxyIntegrationResponse(
+    500,
+    JSON.stringify({
+      message: "An error occurred.",
+    })
+  );
+};
