b05cd02687cddf5dedbb4eed6ed11e7b03647374
Corey Schnedl <schnedl.2@osu.edu>
2023-04-14 10:00:21 -0400

    converting method to stage variable

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -3,26 +3,38 @@ import {
   Cors,
   IResource,
   LambdaIntegration,
-  MethodLoggingLevel,
   RestApi,
 } from "aws-cdk-lib/aws-apigateway";
 import { IFunction } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 import * as path from "path";
 import { APILambdaFunction } from "./api-lambda-function";
+import { CustomAPIGatewayMethod } from "./custom-api-gateway-method";
 
 export interface RestAPIStackProps extends StackProps {
   environment: string;
+}
+
+interface AddApiResourceProps {
+  parentResource: IResource;
+  resourceName: string;
+  methods: string[];
+  handler: IFunction;
 }
 
 export class RestAPIStack extends Stack {
   constructor(scope: Construct, id: string, props: RestAPIStackProps) {
     super(scope, id, props);
 
+    const stageName = props?.environment.toLowerCase().replace(/-/g, "");
+
     const api = new RestApi(this, "RestAPI", {
       restApiName: `RESTApi-${props.environment}`,
       deployOptions: {
-        stageName: props?.environment.toLowerCase().replace(/-/g, ""),
+        stageName,
+        variables: {
+          lambdaAliasName: stageName,
+        },
       },
       defaultCorsPreflightOptions: {
         allowOrigins: Cors.ALL_ORIGINS,
@@ -38,31 +50,34 @@ export class RestAPIStack extends Stack {
       "CalculationsLambda",
       {
         entry: `${path.resolve(__dirname)}/lambdas/calculations/index.ts`,
-        aliasName: props.environment,
+        aliasName: stageName,
       }
     );
 
-    addLambdaBackedEndpoint({
+    this.addLambdaBackedEndpoint({
       parentResource: api.root,
       resourceName: "orders",
       methods: ["POST"],
       handler: calculationsLambda.alias,
     });
   }
-}
 
-interface AddApiResourceProps {
-  parentResource: IResource;
-  resourceName: string;
-  methods: string[];
-  handler: IFunction;
-}
+  private addLambdaBackedEndpoint = (props: AddApiResourceProps) => {
+    const newResource = props.parentResource.addResource(props.resourceName);
 
-function addLambdaBackedEndpoint(props: AddApiResourceProps) {
-  const newResource = props.parentResource.addResource(props.resourceName);
+    for (const method of props.methods) {
+      /**
+       * Adding custom method, since we want to use stage variables to invoke different aliases
+       * This functionality is not currently supported by the AWS CDK construct LambdaIntegration.
+       */
+      new CustomAPIGatewayMethod(this, `${props.resourceName}-${method}`, {
+        method,
+        resourceId: props.parentResource.resourceId,
+        restApiId: props.parentResource.api.restApiId,
+        lambdaArn: props.handler.functionArn,
+      });
+    }
 
-  for (const method of props.methods) {
-    newResource.addMethod(method, new LambdaIntegration(props.handler));
-  }
-  return newResource;
+    return newResource;
+  };
 }
