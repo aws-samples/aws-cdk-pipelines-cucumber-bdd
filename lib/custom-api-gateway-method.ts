06eb260092be83ed223117b0d6c5b458555df686
Corey Schnedl <cschnedl@amazon.com>
2023-04-14 10:00:21 -0400

    converting method to stage variable

---
+++ lib/custom-api-gateway-method.ts
@@ -0,0 +1,38 @@
+import { Aws } from "aws-cdk-lib";
+import { CfnMethod } from "aws-cdk-lib/aws-apigateway";
+import { Construct } from "constructs";
+
+export interface CustomAPIGatewayMethodProps {
+  method: string;
+  resourceId: string;
+  restApiId: string;
+  lambdaArn: string;
+}
+
+export class CustomAPIGatewayMethod extends Construct {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: CustomAPIGatewayMethodProps
+  ) {
+    super(scope, id);
+
+    const lambdaArnWithStageVariable = props.lambdaArn
+      .split(":")
+      .map((val, i) => {
+        return i === 7 ? "${stageVariables.lambdaAliasName}" : val;
+      })
+      .join(":");
+
+    new CfnMethod(scope, "ApiGatewayMethod", {
+      httpMethod: props.method,
+      integration: {
+        integrationHttpMethod: props.method,
+        uri: `arn:${Aws.PARTITION}:${Aws.REGION}:lambda:path/2015-03-31/functions/${lambdaArnWithStageVariable}/invocations`,
+        type: "AWS_PROXY",
+      },
+      resourceId: props.resourceId,
+      restApiId: props.restApiId,
+    });
+  }
+}
