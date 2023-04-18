530e274e1f48b2e27a7438626297974cacfbbb2f
Corey Schnedl <cschnedl@amazon.com>
2023-04-18 17:21:48 -0400

    moving secret into construct

--- lib/cognito-test-user.ts
+++ lib/cognito-test-user.ts
@@ -1,5 +1,9 @@
 import { Construct } from "constructs";
 import { DeployEnvironment } from "../types";
+import { Secret } from "aws-cdk-lib/aws-secretsmanager";
+import * as cdknag from "cdk-nag";
+import { Key } from "aws-cdk-lib/aws-kms";
+import { RemovalPolicy } from "aws-cdk-lib";
 
 export interface CognitoTestUserProps {
   deployEnvironment: DeployEnvironment;
@@ -7,7 +11,28 @@ export interface CognitoTestUserProps {
 }
 
 export class CognitoTestUser extends Construct {
+  public readonly testUserPasswordSecret: Secret;
+
   constructor(scope: Construct, id: string, props: CognitoTestUserProps) {
     super(scope, id);
+
+    /**
+     * Store Test User Password in Secrets Manager
+     */
+    const testUserPassword = new Secret(this, "TestUserPassword", {
+      encryptionKey: new Key(this, "CucumberTestUserPasswordEncKey", {
+        enableKeyRotation: true,
+        removalPolicy: RemovalPolicy.DESTROY,
+      }),
+    });
+
+    cdknag.NagSuppressions.addResourceSuppressions(testUserPassword, [
+      {
+        id: "AwsSolutions-SMG4",
+        reason: "Automatic secret rotation is not valid for Cognito Users.",
+      },
+    ]);
+
+    this.testUserPasswordSecret = testUserPassword;
   }
 }
