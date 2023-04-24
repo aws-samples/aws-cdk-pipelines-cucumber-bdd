6437421c2ceabfe1b941cb843d3cfcfa5e4286cd
Corey Schnedl <cschnedl@amazon.com>
2023-04-24 09:27:56 -0400

    initial deploy script

--- initial-deploy.sh
+++ initial-deploy.sh
@@ -1,5 +1,6 @@
 npm install -g aws-cdk
 npm install
+npx cdk bootstrap --require--approval=never
 npx cdk deploy --all --require-approval=never
 git remote set origin codecommit::us-east-1://aws-cdk-pipelines-cucumber-bdd
 git checkout -B main
